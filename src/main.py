38cb53fffe3a361a9aaa2939b3d74f4e17b9a2a6
myagko <thonyaerevan@gmail.com>
2025-08-31 21:43:56 +0700

    redo app

--- src/main.py
+++ src/main.py
@@ -9,8 +9,47 @@ gi.require_version("AstalHyprland", "0.1")
 gi.require_version("AstalNotifd", "0.1")
 gi.require_version("AstalTray", "0.1")
 
-from gi.repository import GLib
-from app import App
+import os
+import subprocess
+from gi.repository import GLib, Gdk
+from application import Application
+from ui.bar import Bar
+from ui.notifications import Notifications
+from ui.launcher import Launcher
+from ui.powermenu import Powermenu
+
+class App(Application):
+    def do_activate(self):
+        self.hold()
+
+        scss = os.path.expanduser("~/.config/astel-shell/index.scss")
+        css = "/tmp/style.css"
+        subprocess.run(["sass", scss, css])
+        self.apply_css(css, False)
+
+        display = Gdk.Display.get_default()
+        for i in range(display.get_n_monitors()):
+            monitor = display.get_monitor(i)
+            self.add_window(Bar(monitor))
+
+        self.add_window(Notifications())
+
+        launcher = Launcher()
+        powermenu = Powermenu()
+
+        def on_launcher_visible(*_):
+            if launcher.get_visible():
+                powermenu.hide()
+
+        def on_powermenu_visible(*_):
+            if powermenu.get_visible():
+                launcher.hide()
+
+        launcher.connect("notify::visible", on_launcher_visible)
+        powermenu.connect("notify::visible", on_powermenu_visible)
+
+        self.add_window(launcher)
+        self.add_window(powermenu)
 
 if __name__ == "__main__":
     app = App()

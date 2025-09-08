dd8d04511fd1515615d80e06e54c31b86f530b3a
myagko <thonyaerevan@gmail.com>
2025-09-09 01:28:47 +0700

    update

--- src/ui/bar/clients.py
+++ src/ui/bar/clients.py
@@ -3,25 +3,25 @@ import widgets as Widgets
 
 class ClientButton(Widgets.Button):
     def __init__(self, c):
-        hyprland = AstalHyprland.get_default()
+        def on_setup(self):
+            hyprland = AstalHyprland.get_default()
 
-        def on_focused_client(*_):
-            if c == hyprland.get_focused_client():
-                self.add_css_class("focused")
-            else:
-                self.remove_css_class("focused")
+            def on_focused_client(*_):
+                if c == hyprland.get_focused_client():
+                    self.add_css_class("focused")
+                else:
+                    self.remove_css_class("focused")
 
-        def on_focused_workspace(*_):
-            self.set_visible(c.get_workspace() == hyprland.get_focused_workspace())
+            def on_focused_workspace(*_):
+                self.set_visible(c.get_workspace() == hyprland.get_focused_workspace())
 
-        def on_clicked(*_):
-            if c != hyprland.get_focused_client():
-                c.focus()
+            def on_clicked(*_):
+                if c != hyprland.get_focused_client():
+                    c.focus()
 
-            if c.get_floating():
-                hyprland.dispatch("alterzorder", f"top, {c.get_address()}")
+                if c.get_floating():
+                    hyprland.dispatch("alterzorder", f"top, {c.get_address()}")
 
-        def on_button_setup(self):
             on_focused_c_id = hyprland.connect("notify::focused-client", on_focused_client)
             on_focused_ws_id = hyprland.connect("notify::focused-workspace", on_focused_workspace)
             on_c_moved_id = hyprland.connect("client-moved", on_focused_workspace)
@@ -39,7 +39,7 @@ class ClientButton(Widgets.Button):
 
         super().__init__(
             name = "client-button",
-            setup = on_button_setup,
+            setup = on_setup,
             child = Widgets.Label(
                 max_width_chars = 15,
                 ellipsize = Pango.EllipsizeMode.END,
